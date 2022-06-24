8f429cea702319a6547d28ce40392bfe877cd39f
dev-ayaa <ayaaakinleye@gmail.com>
2022-06-24 12:12:00 +0100

    update the signup handler function

--- controller/controller.go
+++ controller/controller.go
@@ -1,7 +1,18 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/yusuf/gin-gonic-ecommerce/model"
+	"github.com/yusuf/gin-gonic-ecommerce/token"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 //HashPassword to hashed the user typed in password
 func HashPassword(password string) string {
@@ -14,19 +25,90 @@ func VerifyPassword(userPassword, hashedPassword string) (bool, string) {
 	return true, ""
 }
 
-//SignUp handlers
-func SignUp() *gin.HandlerFunc {
+//SignUp allow the user to signup handlers
+func SignUp() gin.HandlerFunc {
 
-	return nil
+	return func(c *gin.Context) {
+		var user model.User
+
+		ctx, cancelCtx := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancelCtx()
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		//checking validation for the user models
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+		}
+
+		//checking for used email
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": errors.New("User already existed")})
+		}
+
+		//checking for used phone number
+		count, err = UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+
+		defer cancelCtx()
+
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": errors.New("this phone number is already used")})
+		}
+
+		password := HashPassword(*user.Password)
+		token, refresh_token, _ := generate.TokenGenerator(*user.FirstName, *user.LastName, *user.Email)
+
+		user.Password = &password
+		user.ID = primitive.NewObjectID()
+		user.UserID = user.ID.Hex()
+		user.CreatedAt, _ = time.Parse("RFC3339", time.Now().Format(time.RFC3339))
+		user.UpdatedAt, _ = time.Parse("RFC3339", time.Now().Format(time.RFC3339))
+		user.AddressDetails = make([]model.Address, 0)
+		user.OrderStatus = make([]model.Order, 0)
+		user.UserCart = make([]model.ProductUser, 0)
+		user.Token = &token
+		user.RefreshToken = &refresh_token
+		
+		_, insertErr := UserCollection.InsertOne(ctx, user)
+		if insertErr != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"user cannot be created or insert into database"})
+			return
+		}
+		defer cancelCtx()
+
+		c.JSON(http.StatusCreated, "Signed in successfully")
+
+	}
 }
 
 //Login handlers
-func Login() *gin.HandlerFunc {
+func Login() gin.HandlerFunc {
 
-	return nil
+	return func(c *gin.Context){
+
+		ctx, cancelCtx := context.WithTimeout(context.Background(), 100 * time.Second)
+		defer cancelCtx()
+
+
+	}
 }
 
-//AdminAddProduct 
+//AdminAddProduct
 func AdminAddProduct() *gin.HandlerFunc {
 	return nil
 }
@@ -38,4 +120,3 @@ func SearchProduct() *gin.HandlerFunc {
 func SearchProductByQuery() *gin.HandlerFunc {
 	return nil
 }
-
