40deb4d6fefeab0001fab5c28d2de03f496ced65
dev-ayaa <ayaaakinleye@gmail.com>
2022-06-24 13:00:47 +0100

    checking for valid login

--- controller/controller.go
+++ controller/controller.go
@@ -8,15 +8,20 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gobuffalo/validate/v3/validators"
 	"github.com/yusuf/gin-gonic-ecommerce/model"
 	"github.com/yusuf/gin-gonic-ecommerce/token"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+var UserCollection *mongo.Collection
 
 //HashPassword to hashed the user typed in password
 func HashPassword(password string) string {
-	return ""
+
 }
 
 //VerifyPassword to Verify the user password  with respect to the hashed password
@@ -34,6 +39,7 @@ func SignUp() gin.HandlerFunc {
 		ctx, cancelCtx := context.WithTimeout(context.Background(), 100*time.Second)
 		defer cancelCtx()
 
+		//Marshalling and UnMarshalling the user struct model
 		if err := c.BindJSON(&user); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
@@ -83,10 +89,10 @@ func SignUp() gin.HandlerFunc {
 		user.UserCart = make([]model.ProductUser, 0)
 		user.Token = &token
 		user.RefreshToken = &refresh_token
-		
+
 		_, insertErr := UserCollection.InsertOne(ctx, user)
-		if insertErr != nil{
-			c.JSON(http.StatusInternalServerError, gin.H{"error":"user cannot be created or insert into database"})
+		if insertErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "user cannot be created or insert into database"})
 			return
 		}
 		defer cancelCtx()
@@ -99,14 +105,36 @@ func SignUp() gin.HandlerFunc {
 //Login handlers
 func Login() gin.HandlerFunc {
 
-	return func(c *gin.Context){
-
-		ctx, cancelCtx := context.WithTimeout(context.Background(), 100 * time.Second)
+	return func(c *gin.Context) {
+		var user model.User
+		ctx, cancelCtx := context.WithTimeout(context.Background(), 100*time.Second)
 		defer cancelCtx()
 
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
-	}
-}
+		//Checking if the user exist in the database
+		err := UserCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&existingUser)
+		defer cancelCtx()
+		if err != nil{
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"incorrect login email"})
+			return
+		}
+		validPassword, passwordMsg := VerifyPassword(*user.Password,*foundUser.Password )
+		defer cancelCtx()
+		if !validPassword{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":passwordMsg})
+			fmt.Println(passwordMsg)
+			return
+		}
+
+		token, refresh_token, _ := generate.TokenGenerator(*foundUser.FirstNAme, *foundUser.LastName, *foundUser)
+		defer cancelCtx()
+		
+		generate.UpdateAllToken(token, refresh_token)
+		c.JSON(http.StatusFound, foundUser)
 
 //AdminAddProduct
 func AdminAddProduct() *gin.HandlerFunc {
